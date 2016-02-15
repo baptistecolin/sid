51444077104f85504f2367ba7d815d86961ee4ed
Alban Depretz <alban.depretz@mines-paristech.fr>
2016-02-15 14:21:04 +0100

    server_connection made abstract

--- server_connection.py
+++ server_connection.py
@@ -40,45 +40,18 @@ class server_connection(object):
 	def __getitem__(self,x) :
 		pass
     		#x.__getitem__(y)<=>x[y]
-
+	
+	@abstractmethod
 	def __setitem__(self, key, value, /) :
 		pass
     		#set self[key] to value
-
+	
+	@abstractmethod
 	def __new__(self,*args, **kargs) :
 		pass
     		#pour utiliser la syntaxe connection = new connection()
  
-=======
-class server_connection
 
-def put(k, v) :
-    #créer sur le serveur le fichier envoyé, ou le modifier s'il existe déjà
     
 
-def get(k) :
-    #récupérer le fichier à partir de sa clé (hash du nom)
-    
-def delete(k) :
-    #supprimer le fichier à partir de sa clé
 
-
-
-def  __contains__(self, key, /) :
-    #histoire qu'on puisse utiliser "if key in list"
-
-def __delitem__(self, key, /) :
-    #pour que delete self[key] ait un sens
-
-def __eq__(self, value, /) :
-    #afin que self==value marche
-
-def __getitem__(x) :
-    #x.__getitem__(y)<=>x[y]
-
-def __setitem__(self, key, value, /) :
-    #set self[key] to value
-
-def __new__(*args, **kargs) :
-    #pour utiliser la syntaxe connection = new connection()
->>>>>>> dabea6d73f1e385343ebb800a532f78c4a616c05
