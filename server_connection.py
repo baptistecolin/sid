a328debb5709aa36975cd9baa142dcd8ae2f080a
Haroun BELTAIFA <Haroun@wifi-mpt-053.paris.ensmp.fr>
2016-02-15 14:04:58 +0100

    classe abstraite connection serveur, plus connection locale

---
+++ server_connection.py
@@ -0,0 +1,35 @@
+#coding=utf-8
+
+class server_connection
+
+def put(k, v) :
+    #créer sur le serveur le fichier envoyé, ou le modifier s'il existe déjà
+    
+
+def get(k) :
+    #récupérer le fichier à partir de sa clé (hash du nom)
+    
+def delete(k) :
+    #supprimer le fichier à partir de sa clé
+
+
+
+def  __contains__(self, key, /) :
+    #histoire qu'on puisse utiliser "if key in list"
+
+def __delitem__(self, key, /) :
+    #pour que delete self[key] ait un sens
+
+def __eq__(self, value, /) :
+    #afin que self==value marche
+
+def __getitem__(x) :
+    #x.__getitem__(y)<=>x[y]
+
+def __setitem__(self, key, value, /) :
+    #set self[key] to value
+
+def __new__(*args, **kargs) :
+    #pour utiliser la syntaxe connection = new connection()
+
+def 
