3fa1d58c891c762d552a701c187bccaaf3b830a8
baptiste <baptiste.colin@mines-paristech.Fr>
2016-02-16 12:30:17 +0100

    sidcrypto

---
+++ SIDCrypto.py
@@ -0,0 +1,125 @@
+from Crypto.Cipher import AES,ARC4
+from Crypto.Hash import MD5,SHA256,SHA512
+from os import urandom
+import sys
+from Crypto import Random
+
+class SIDCrypto:
+    def __init__(self, password, algo_cipher=AES, cipher_mode=AES.MODE_CBC, algo_hash = "SHA256", keylen=16, ivlen=16, saltlen=8):
+        self.algo_cipher = algo_cipher
+        self.cipher_mode = cipher_mode
+        
+        b = bytearray()
+        b.extend(map(ord, password))
+        self.password = b
+
+        self.algo_hash = algo_hash
+        self.keylen = keylen
+        self.ivlen = ivlen
+        self.saltlen = saltlen
+
+    def key_iv_salt_generator(self,password):
+        rand = Random.new()
+        iv = rand.read(self.ivlen) #random generation of the iv
+        salt = rand.read(self.saltlen) #random generation of the salt
+        password_bytes = password.encode('utf-8')
+
+        key = self.hash(password_bytes+salt , converting_bytes = True) #the key is the hash of the password+the salt
+
+        return (key,iv,salt)        
+
+
+
+###ENCRYPTION FUNCTION
+    def encrypt(self, path, keylen=16, ivlen=16, saltlen=8): #the path is the one of the file that will be ciphered
+        
+        (key,iv,salt) = self.key_iv_salt_generator(password)
+       
+        cipher = AES.new(key, self.cipher_mode, iv) #a cipher is generated
+        
+        #print(AES.block_size)
+
+        o = open(path, 'rb')
+        c = cipher.encrypt(o.read()) #the file is ciphered
+        o.close()
+        
+        c += iv #the iv is appended to the ciphered message
+        c += salt #the salt is appended tot he ciphered message after the iv
+        
+        print(c)
+        print(iv)
+        print(salt)
+
+        return c #the output is a string containing the ciphered message + the encrypted iv
+
+
+###DECRYPTION FUNCTION
+    def decrypt(self,path,password):
+        
+        o = open(path, 'rb')
+        c = o.read()
+
+        m = self.decryptString(c,password)
+
+        return m #the output is a string containing the message.
+
+    def decryptString(self,s,password):
+        
+        if (self.algo_cipher==None):
+            return s           #it is possible not to encrypt anything by assigning "None" to "algo_cipher". Useful for debugging.
+
+        else:
+            c , iv, salt = s[:len(s)-(self.saltlen + self.ivlen)] , s[len(s)-(self.ivlen+self.saltlen):len(s)-self.saltlen] , s[len(s)-self.saltlen:] #the string is splitted into the actual ciphered message, the iv, and the salt
+            password_bytes = password.encode('utf-8')
+            key = self.hash(password_bytes+salt , converting_bytes = True) #the key is the hash of the password+the salt
+
+            cipher = AES.new(key, self.cipher_mode, iv)
+            m = cipher.decrypt(c)
+            
+            print(m)
+            return m #the output is a string containing the message.
+
+###HASH FUNCTION
+    def hash(self, name, version = -1, converting_bytes = False):
+        
+        if (not converting_bytes):
+            if version == -1:
+                s = name
+            else:
+                s = name + str(version)
+            s = s.encode("utf-8")
+        
+        else:
+            s = name
+
+        if self.algo_hash == "MD5":
+            h = MD5.new()
+            
+        elif self.algo_hash == "SHA256":
+            h = SHA256.new()
+            
+        elif self.algo_hash == "SHA512":
+            h = SHA512.new()
+                
+        h.update(s)
+        return h.digest()
+
+rand=Random.new()
+keylen = 16
+
+password = "msi2014"
+sid = SIDCrypto(password)
+
+message_clair ="c9ae3045f221dbfc6b051ed05d64ef4b"
+clear = open("/home/baptiste/msi-p14/clear.txt", 'w')
+clear.write(message_clair)
+
+message_chiffre = sid.encrypt("/home/baptiste/msi-p14/clear.txt")
+
+encrypted = open("/home/baptiste/msi-p14/encrypted.txt", 'w')
+encrypted.write(message_chiffre)
+encrypted.close()
+encrypted = open("/home/baptiste/msi-p14/encrypted.txt", 'r')
+
+decrypted = open("/home/baptiste/msi-p14/decrypted.txt", 'w')
+decrypted.write(sid.decrypt("/home/baptiste/msi-p14/encrypted.txt", 'msi2014'))
