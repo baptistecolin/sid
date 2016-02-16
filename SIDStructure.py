5aae906d825b0d48b4fa91decc2fa37e222e0afb
legrand <amaury.legrand@mines-paristech.fr>
2016-02-15 23:56:55 -0800

    SIDStructure.py update : new functions, bugfixes

--- SIDStructure.py
+++ SIDStructure.py
@@ -4,21 +4,23 @@ import json
 #import SIDCrypto
 #import sid
 
-#crypto = SIDCrypto("", "", "") # define ??
+#crypto = SIDCrypto("", "orage", "") # define ??
 
 ## Recursively lists all files in directory "path" (with included path)
+# /!\ Tested on Linux systems only (incompatible with Windows)
 # @path : str
 def listFiles(path):
 	files = []
 	l = glob.glob(path + "/*")
 	for i in l:
+		print(i)
 		if os.path.isdir(i):
 			files.extend(listFiles(i))
-		elif i != (path + "/last.sid"):
+		elif not i.endswith(".sid"):
 			files.append(i)
 	return files
 
-print(listFiles("."))
+print(listFiles(""))
 
 ## Generate "last.sid" file from version "ver" in directory "path"
 # @path : str
@@ -60,12 +62,15 @@ def buildSID(path, isNew = False):
 	return to_upload
 """
 
-def buildSID(path, isNew = False):
+def buildSID(path = ".", isNew = False):
 	to_upload = []
 	dic = {}
 	dic["files"] = {}
 	if not isNew:
-		last_info = json.load() #decrypt + download
+		o = open("last.sid", "wb")		
+		o.write(sid.protocol.get("last.sid")) # !! nom
+		o.close()
+		last_info = json.loads(crypto.decrypt("last.sid"))
 		ver = last_info["version"] + 1
 		id_max = last_info["id_max"]
 	else:
@@ -73,8 +78,10 @@ def buildSID(path, isNew = False):
 		id_max = 0
 	dic["version"] = ver
 	for f in listFiles(path):
-		name = f.rsplit("\\", 1)[-1]
-		fcontent = open(f, "rb").read()
+#		name = f.rsplit("/", 1)[-1]
+		o = open(f, "rb")
+		fcontent = o.read()
+		o.close()
 		fhash = crypto.hash(fcontent)
 		if isNew:
 			dic["files"][f] = {"serverName" : id_max,
@@ -96,12 +103,73 @@ def buildSID(path, isNew = False):
 						"hash" : fhash}
 				id_max += 1
 	if to_upload:
+		if not isNew:
+			# rename previous
+			o = open("last.sid", "rb")
+			prevSid = "v" + last_info["version"] + ".sid"
+			prev = open(prevSid, "wb")
+			prev.write(o.read())
+			o.close()
+			prev.close()
+		# create new
 		dic["id_max"] = id_max
-		js = json.dumps(dic)
-		js = crypto.encrypt(js)
-		open("last.sid", "wb").write(js)
-	return to_upload
-	 
+		json.dump(dic, "last.sid")
+		js = crypto.encrypt("last.sid")
+		o = open("last.sid", "wb")
+		o.write(js)
+		o.close()
+		to_upload.append("last.sid")
+		to_upload.append(prevSid)
+	return to_upload, dic["files"]
+
+
+def SIDSave(path = "."):
+	to_upload, dic = buildSID(path)
+	for f in to_upload:
+		o = open(f, "rb")
+		sid.protocole.put(dic[f]["serverName"], crypto.encrypt(o.read())) # !! nom
+		o.close()
+
+def SIDCreate(path = "."):
+	to_upload, dic = buildSID(path, True)
+	for f in to_upload:
+		o = open(f, "rb")
+		sid.protocole.put(dic[f]["serverName"], crypto.encrypt(o.read())) # !! nom
+		o.close()
+		
+
+def SIDRestore(ver = -1, path = "."):
+	downloaded = []
+
+	if ver < 0:
+		lastSID = json.loads(crypto.decryptString(sid.protocol.get("last.sid")))
+		o = open("last.sid", "wb")
+		o.write(lastSID) # keep track on local machine
+		o.close()
+	else:
+		lastSID = json.loads(crypto.decryptString(sid.protocol.get("v" + ver + ".sid")))
+		o = open("last.sid", "wb")
+		o.write(lastSID) # keep track on local machine
+		o.close()
+
+	for f, v in lastSID["files"].items():
+		o = open(f, "rb")
+		fcontent = o.read()
+		o.close()
+		fhash = crypto.hash(fcontent)
+		if fhash != v["hash"]:
+			flux = sid.protocol.get(v["serverName"])
+			o = open(f, "wb")
+			o.write(crypto.decryptString(flux))
+			o.close()
+			downloaded.append(f)
+
+	return downloaded
+	
+
+	
+
+
 
 
 
