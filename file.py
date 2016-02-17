39188d0315f4b18e510b993dec13886beaf32c27
Alban Depretz <alban.depretz@mines-paristech.fr>
2016-02-17 14:52:38 +0100

    Improved file.py

--- file.py
+++ file.py
@@ -11,7 +11,6 @@ class File(server_connection):
 		self.savePath=path
 		if not os.path.exists(path):
 			os.makedirs(path)
-		
 
 	def put(self,k,v):
 		fileServer=open(os.path.join(self.savePath,k),'wb')
@@ -19,24 +18,22 @@ class File(server_connection):
 		fileServer.close()
     
 	def get(self,k):
-		fileContent=open(os.path.join(self.savePath,k),'rb')
-		if fileContent:
-			return fileContent
-		else:
-			print("Pas de fichier de ce nom sur le serveur")
-			return None
+		try:
+			return open(os.path.join(self.savePath,k),'rb').read()
+		except FileNotFoundError:
+			return 'No such files'
 
 	def delete(self,k):
 		for root, dirs, files in os.walk(self.savePath):
 			if k in files:
 				os.remove(os.path.join(root,k))
-				print ("le fichier %s  a ete supprime"%k)
+				print ("The file %s was deleted!"%k)
 #Testing the above functions
 def main():
 	fileSystem=File('/home/alban/Documents/test/')
 	test=open("test.txt", 'rb')
 	fileSystem.put('1234',test.read())
-	print(fileSystem.get('1234').read())
+	print(fileSystem.get('12345'))
 	fileSystem.delete('1234')
 if __name__=='__main__':
 	main()
