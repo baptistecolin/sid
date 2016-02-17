88705b7e47490cdaa8e5853f76df192aefe8aaba
Adrien Tardieu <adrien.tardieu@mines-paristech.fr>
2016-02-17 11:21:30 +0100

    encrypted cach handling

--- cach.py
+++ cach.py
@@ -13,15 +13,14 @@ cach_dir = os.environ['HOME'] + '/.sid'
 def save(name,crypto,url='',directory_path='',version=0):
     if not os.path.isdir(cach_dir):
        os.mkdir(cach_dir) 
-    #if name in os.listdir(cach_dir): 
-        #(v,url,directory_path) = read_save(name,crypto)
+    if name in os.listdir(cach_dir): 
+        (v,url,directory_path) = read_save(name,crypto)
     dic = {'version' : version,
         'url' : url,
         'directory_path' : directory_path}
     t = os.path.join(cach_dir, name)
-    crypted_json = json.dumps(dic)
-    print(type(crypted_json))
-    o = open(t,"w")
+    crypted_json = crypto.encryptBytes(bytes(json.dumps(dic).encode('ASCII')))
+    o = open(t,"wb")
     o.write(crypted_json)
     o.close()
 
@@ -44,16 +43,16 @@ def read_save(name,crypto):
             print("No save with this name")
         else:
             t = cach_dir + '/' + name
-            o = open(t,"r")
-            
+            o = open(t,"rb")
+            found = o.read() 
             o.close()
-            j_dic = json.loads(o.read())
+            j_dic = json.loads(crypto.decryptBytes(found).decode('ASCII'))
             print(j_dic)
             return (j_dic['version'],j_dic['url'],j_dic['directory_path']) #return (version,url,directory_address) 
 
-cryptoEx = SIDCrypto("adrien")
-save('save0',cryptoEx,'https://www.google.com/','/home/adrien/Documents/School/Mines/MSI/Cours/msi-p14/dossierTest')
-list_saves()
+#cryptoEx = SIDCrypto("adrien")
+#save('save0',cryptoEx,'https://www.google.com/','/home/adrien/Documents/School/Mines/MSI/Cours/msi-p14/dossierTest')
+#list_saves()
 #read_save('save0',cryptoEx)
 #save('save0',cryptoEx,version=1)
 #read_save('save0',cryptoEx)
