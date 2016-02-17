2253e5e446e901d19c6a6541bc5d02f1a571c383
xiangnanyue <xiangnan.yue@mines-paristech.fr>
2016-02-17 17:55:12 +0100

    separate avec get et put done

--- separate.py
+++ separate.py
@@ -4,34 +4,47 @@ import math
 import io
 
 class Separate():
-	
+
     def __init__(self, store):
     	self.MAX = 10
     	self.store = store
     	print('MAX bytes per block: ', self.MAX)
     
     def get(self, k):
-    	if k in store:
-        	return store.get(k)
+        if k in self.store:
+             return self.store.get(k)
         content = []
         i = 0
         while True:
-        	key = k + bytes(str(i), 'utf8')		# regular: k + b'0'...
-        	if key not in store: 
-        		break
-        	content.append(store.get(key))			# get result sont bytes
-            i+=1
-            
+            key = k + bytes(str(i), 'utf8')   # regular: k + b'0'...
+            if key not in self.store: 
+                break
+            content.append(self.store.get(key))   # get result sont bytes
+            i = i + 1
         if len(content) == 0:
-        	print("Failed to find the clé ", key)
-			return None    
-		return content	
-    
+            print("Failed to find the clé ", key)
+            return None    
+        return b''.join(content)
+        
+    def put(self, k, v):
+        n = len(v)
+        num = n // self.MAX
+        for i in range(num):
+            self.store.put(k+ bytes(str(i), 'utf8'), v[self.MAX * i : self.MAX*(i + 1)])
+        if n%self.MAX != 0:
+            self.store.put(k + bytes(str(num), 'utf8'), v[self.MAX * num: ])
+		
+		
+		
+		
 if __name__ == '__main__':
-	password = getpass.getpass("enter password: ")
-	IM = Imaps('xiangnan.chat@gmail.com', password, name=b'test')
-	for i in range(5):
-		IM.put(b'toto' + bytes(str(i), utf8), b'blablablablablabla')
+	#password = getpass.getpass("enter password: ")
+	IM = Imaps('xiangnan.chat@gmail.com', 'www3.141592653', name=b'test')
+	#for i in range(5):
+		#IM.put(b'toto' + bytes(str(i), 'utf8'), b'blablablablablabla')
+		
 	sp = Separate(IM)
-	sp.get('toto')
+	sp.put(b'totoro', b'blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla')
+	print(sp.get(b'totoro'))
+	
     	
