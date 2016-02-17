d3c4baabab5536031e112500bb5a46a57dc9c737
Haroun <haroun.beltaifa@mines-paristech.fr>
2016-02-17 15:05:41 +0100

    Modification separate.py

--- separate.py
+++ separate.py
@@ -5,13 +5,16 @@ import io
 
 
 class Separate():
-    def __init__(self, k, v):
+    def __init__(self, storage, maxsize=1000000):
+        self.maxsize = maxsize
+        self.storage = storage
+
+    def put(self, k, v):
         self.content = []
         n = len(v)
-        block = io.BytesIO()
         i = 0
-        while i <= math.ceil(n / 1000000):
-            block = v[1000000 * i:1000000 * (i + 1)]
-            self.content += block
+        while i < n // self.maxsize:
+            self.content.append(v[self.maxsize * i:self.maxsize * (i + 1)])
             i+=1
-        return(k, self.content)
+        self.content.append(v[self.maxsize * i:])
+        # k ?
