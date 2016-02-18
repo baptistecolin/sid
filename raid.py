b223460373ba227fb61f1be34f4bbb310c114b8e
Alban Depretz <alban.depretz@mines-paristech.fr>
2016-02-18 12:11:10 +0100

    hop

--- raid.py
+++ raid.py
@@ -1,32 +1,39 @@
 # -*- coding:utf-8 -*-
 
+from file import *
 
 class Raid():
-    def __init__(self, version, store):
-        if int(version)==10 :
-            self.version=int(version)
-        self.store=store
+	def __init__(self, version=10):
+		if int(version)==10 :
+			self.version=int(version)
 
-    def getKey(self,k,i):
-        return '%s--%s'%(k,i)
+	def getKey(self,k,i):
+		return '%s--%s'%(k,i)
 
-    def put(self, k, v):
-        n = len(v)
-        disk1 = self.store("./"+k+'-1')
-        disk2 = self.store("./"+k+'-2')
-        disk3 = self.store("./"+k+'-3')
-        disk4 = self.store("./"+k+'-4')
-        parts_number=8
-        parts = []
-        i=0
-        while i<parts_number:
-            parts.append(v[n // parts_number * i : n // parts_number * (i + 1)])
-            i+=1
-        parts.append(v[n // parts_number * parts_number:])
-        for i in range(0,8):
-            if i%2==0:
-                disk1.put(self.getKey(k,i), parts[i])
-                disk3.put(self.getKey(k,i), parts[i])
-            else :
-                disk2.put(self.getKey(k,i), parts[i])
-                disk4.put(self.getKey(k,i), parts[i])
+	def put(self, k, v):
+
+		disk1=File('/home/alban/raid1/%s-1/'%k)
+		disk2=File('/home/alban/raid1/%s-2/'%k)
+		disk3=File('/home/alban/raid1/%s-3/'%k)
+		disk4=File('/home/alban/raid1/%s-4/'%k)
+
+		n=len(v)
+		parts_number=7
+		parts = []
+
+		for i in range(parts_number):
+			parts.append(v[n // parts_number * i : n // parts_number * (i + 1)])
+		parts.append(v[n // parts_number * parts_number:])
+
+		for i in range(parts_number):
+			if i%2==0:
+				disk1.put(self.getKey(k,i), parts[i])
+				disk3.put(self.getKey(k,i), parts[i])
+			else :
+				disk2.put(self.getKey(k,i), parts[i])
+				disk4.put(self.getKey(k,i), parts[i])
+def main():
+	raid=Raid()
+	raid.put('helloworld',b'blablablabalbalbalbalbalablabalbalbalabalbalbalabalbalablabalbalbalbalablabalbalbalbalbalablabalbalbalbalablabalbalbalbalbalablablablablabalbalblabalbalbalablab')
+if __name__=='__main__':
+	main()
